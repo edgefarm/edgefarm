a013edaef9430a231ae4bcaaed49a12abacdeb64
Florian Reinhold <florian.reinhold@ci4rail.com>
2021-11-24 15:54:15 +0100

    a lot of stuff

--- hack/instantiate-kubeedge-node.sh
+++ hack/instantiate-kubeedge-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CLOUDCORE_ADDRESS=${1}
 NODE_NAME=${2}
-docker run -d --rm --env CLOUDCORE_ADDRESS=${CLOUDCORE_ADDRESS} --env NODE_NAME=${NODE_NAME} --name ${NODE_NAME} \
+docker run -d --env CLOUDCORE_ADDRESS=${CLOUDCORE_ADDRESS} --env NODE_NAME=${NODE_NAME} --name ${NODE_NAME} \
 -v $(pwd)/dev/manifests/kubeedge-certs/config/rootCa.pem:/etc/kubeedge/certs/rootCa.pem \
 -v $(pwd)/dev/manifests/kubeedge-certs/config/node.pem:/etc/kubeedge/certs/node.pem \
 -v $(pwd)/dev/manifests/kubeedge-certs/config/node.key:/etc/kubeedge/certs/node.key \
