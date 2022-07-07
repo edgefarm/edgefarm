d6ecd602e64981e8fd196921c52cf05ff3435eea
KlausPopp <klauspopp@gmx.de>
2022-07-07 15:27:08 +0200

    app tests

--- test/e2e/apps/01_multi.go
+++ test/e2e/apps/01_multi.go
@@ -31,4 +31,21 @@ var _ = ginkgo.Describe("Edge Multi App Deployment", func() {
 		l = getFirstPodLog(testingNameSpace, podNamePrefix3, 10)
 		Expect(l).To(ContainSubstring("HelloFromApp2-B"))
 	})
+	ginkgo.It("Multiple apps can be deployed on a many edge nodes", func() {
+		numNodes := 3
+		framework.ExpectNoError(labelNodes(testingNameSpace, numNodes))
+		framework.RunKubectlOrDie(kubeConfig, testingNameSpace, "apply", "-f", "apps/simple.yaml")
+		framework.RunKubectlOrDie(kubeConfig, testingNameSpace, "apply", "-f", "apps/app2.yaml")
+		framework.ExpectNoError(waitPodsAreAppliedToAllSelectedNodes(testingNameSpace, nodeLabelKey, podNamePrefix1, numNodes))
+		l := getFirstPodLog(testingNameSpace, podNamePrefix1, 10)
+		Expect(l).To(ContainSubstring("Hello"))
+
+		framework.ExpectNoError(waitPodsAreAppliedToAllSelectedNodes(testingNameSpace, nodeLabelKey, podNamePrefix2, numNodes))
+		l = getFirstPodLog(testingNameSpace, podNamePrefix2, 10)
+		Expect(l).To(ContainSubstring("HelloFromApp2-A"))
+
+		framework.ExpectNoError(waitPodsAreAppliedToAllSelectedNodes(testingNameSpace, nodeLabelKey, podNamePrefix3, numNodes))
+		l = getFirstPodLog(testingNameSpace, podNamePrefix3, 10)
+		Expect(l).To(ContainSubstring("HelloFromApp2-B"))
+	})
 })
