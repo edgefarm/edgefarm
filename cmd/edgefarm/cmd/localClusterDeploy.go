31ed0e073279773ffcbd97bee3eea40130d30b18
Armin Schlegel <armin.schlegel@gmx.de>
2023-07-17 16:00:10 +0200

    feat: deploying edgefarm.applications

--- cmd/edgefarm/cmd/localClusterDeploy.go
+++ cmd/edgefarm/cmd/localClusterDeploy.go
@@ -1,18 +1,18 @@
-/*
-Copyright 2022 The OpenYurt Authors.
+// /*
+// Copyright 2022 The OpenYurt Authors.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
 
 package cmd
 
@@ -21,9 +21,6 @@ package cmd
 // 	"os"
 
 // 	"github.com/spf13/cobra"
-// 	"k8s.io/klog/v2"
-
-// 	"github.com/edgefarm/edgefarm/pkg/packages"
 // )
 
 // func NewDeployCommand(out io.Writer) *cobra.Command {
@@ -67,10 +64,15 @@ package cmd
 // 	// 	return err
 // 	// }
 
-// 	klog.Infof("Deploy edgefarm network packages")
-// 	if err := packages.Install(packages.Network); err != nil {
-// 		return err
-// 	}
+// 	// klog.Infof("Deploy edgefarm network packages")
+// 	// if err := packages.Install(packages.Network); err != nil {
+// 	// 	return err
+// 	// }
+
+// 	// klog.Infof("Deploy edgefarm applications packages")
+// 	// if err := packages.Install(packages.Applications); err != nil {
+// 	// 	return err
+// 	// }
 
 // 	return nil
 // }
