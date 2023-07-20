427ca2f832e8ce92b4cb7982e9df608e7bb54002
Armin Schlegel <armin.schlegel@gmx.de>
2023-07-20 15:33:43 +0200

    feat: renamed project
    Signed-off-by: Armin Schlegel <armin.schlegel@gmx.de>

--- cmd/local-up/main.go
+++ cmd/local-up/main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/edgefarm/edgefarm/cmd/edgefarm/cmd"
+import "github.com/edgefarm/edgefarm/cmd/local-up/cmd"
 
 func main() {
 	cmd.Execute()
