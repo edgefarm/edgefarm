6c76915f26ce306277a2576bf82eee210a95ccdf
Armin Schlegel <armin.schlegel@gmx.de>
2023-08-03 13:30:37 +0200

    feat: license texts modified
    Signed-off-by: Armin Schlegel <armin.schlegel@gmx.de>

--- cmd/local-up/cmd/cluster.go
+++ cmd/local-up/cmd/cluster.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 EdgeFarm authors
+Copyright © 2023 EdgeFarm Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -29,14 +29,4 @@ var localClusterCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(localClusterCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// localCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// localCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
